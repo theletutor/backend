bf2e11e0f65e01d3cfe4a57549b7eea7ffadbb6b
Abhishek <abhishek26321@gmail.com>
2021-01-23 21:47:52 +0530

    Added user controller and service

---
+++ letutor/master-api/src/main/java/com/letutor/master/controller/v1/UserController.java
@@ -0,0 +1,24 @@
+package com.letutor.master.controller.v1;
+
+import com.letutor.master.dto.response.UserDetailsResponse;
+import com.letutor.master.service.IUserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+  private final IUserService userService;
+
+  public UserController(IUserService userService){
+    this.userService = userService;
+  }
+
+  @GetMapping(value = "/{id}")
+  public UserDetailsResponse getUserDetails(@PathVariable String id){
+    return userService.getUserDetails(id);
+  }
+}
