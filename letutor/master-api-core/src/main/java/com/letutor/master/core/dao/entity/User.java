49a1807ad0f881343484f9fb107bb1f4f5461251
Abhishek <abhishek26321@gmail.com>
2021-01-24 17:48:00 +0530

    Added config for db

--- letutor/master-api-core/src/main/java/com/letutor/master/core/dao/entity/User.java
+++ letutor/master-api-core/src/main/java/com/letutor/master/core/dao/entity/User.java
@@ -1,5 +1,18 @@
 package com.letutor.master.core.dao.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+
+  @Id
+  @Column(name = "user_id")
+  private String userId;
 
 }
