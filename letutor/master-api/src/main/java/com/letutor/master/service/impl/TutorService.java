3b549b4c6a9eaf8aae5209d7bccd3cd7507ecda6
Abhishek <abhishek26321@gmail.com>
2021-01-24 12:02:21 +0530

    Added new services and dependencies

---
+++ letutor/master-api/src/main/java/com/letutor/master/service/impl/TutorService.java
@@ -0,0 +1,18 @@
+package com.letutor.master.service.impl;
+
+import com.letutor.master.dto.response.AllTutorDetailsResponse;
+import com.letutor.master.dto.response.TutorDetailsResponse;
+import com.letutor.master.service.ITutorService;
+
+public class TutorService implements ITutorService {
+
+  @Override
+  public AllTutorDetailsResponse getAllTutorDetails() {
+    return null;
+  }
+
+  @Override
+  public TutorDetailsResponse getTutorDetails(String id) {
+    return null;
+  }
+}
---
+++ letutor/master-api/src/main/java/com/letutor/master/service/impl/UserService.java
@@ -0,0 +1,12 @@
+package com.letutor.master.service.impl;
+
+import com.letutor.master.dto.response.UserDetailsResponse;
+import com.letutor.master.service.IUserService;
+
+public class UserService implements IUserService {
+
+  @Override
+  public UserDetailsResponse getUserDetails(String userId) {
+    return null;
+  }
+}
